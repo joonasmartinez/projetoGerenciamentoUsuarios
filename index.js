0a395e03115429afc244e2cac16c46d0a246020d
Jonas <mz-martinez@live.com>
2022-04-16 22:45:00 -0300

    Alterando forma de retornar informações no Form

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ var name = document.querySelector("#exampleInputName");
 var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
 var birth = document.querySelector("exampleInputBirth");
 var country = document.querySelector("exampleInputCountry");
-var email = document.querySelector("exampleInputEmail1");
-var password = document.querySelector("exampleInputPassword1");
+var email = document.querySelector("exampleInputEmail");
+var password = document.querySelector("exampleInputPassword");
 var photo = document.querySelector("exampleInputFile");
-var admin = document.querySelector("#");
+var admin = document.querySelector("exampleInputAdmin");
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function(field, index){
+
+console.log(field)
+
+});
