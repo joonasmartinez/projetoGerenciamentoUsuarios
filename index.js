bb094cc28ad3b1c98ed0d54b1d468906636c9117
Jonas <mz-martinez@live.com>
2022-04-18 16:08:38 -0300

    Desenvolvendo Orientado Objeto

--- index.js
+++ index.js
@@ -3,23 +3,21 @@ var user = {};
 
 function addLine(dataUser){
 
-let tr = document.createElement("tr");
+    console.log(dataUser);
 
-tr.innerHTML = `<tr>
-<td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-<td>${dataUser.name}</td>
-<td>${dataUser.email}</td>
-<td>${dataUser.admin}</td>
-<td>${dataUser.birth}</td>
-<td>
-  <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-  <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-</td>
-</tr>`;
+    document.getElementById("table-users").innerHTML = `<tr>
+    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+    <td>${dataUser.name}</td>
+    <td>${dataUser.email}</td>
+    <td>${dataUser.admin}</td>
+    <td>${dataUser.birth}</td>
+    <td>
+    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+    </td>
+    </tr>`;
 
-document.getElementById("table-users").appendChild(tr);
-
-}
+    }
 
 document.getElementById("form-user-create").addEventListener("submit", function(event){
 
@@ -37,5 +35,16 @@ document.getElementById("form-user-create").addEventListener("submit", function(
             user[field.name] = field.value;
         }
     });
-    addLine(user);
-})
+
+    var objectUser = new User(
+        user.name, 
+        user.gender, 
+        user.birth, 
+        user.country, 
+        user.email, 
+        user.password, 
+        user.photo, 
+        user.admin
+        );
+    addLine(objectUser);
+});
