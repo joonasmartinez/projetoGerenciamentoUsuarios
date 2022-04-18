01538e734f96a534389d1c4f79a923eb93a7a738
Jonas <mz-martinez@live.com>
2022-04-18 14:54:30 -0300

    Formando JSON formulario

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("exampleInputBirth");
-var country = document.querySelector("exampleInputCountry");
-var email = document.querySelector("exampleInputEmail");
-var password = document.querySelector("exampleInputPassword");
-var photo = document.querySelector("exampleInputFile");
-var admin = document.querySelector("exampleInputAdmin");
-
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
 fields.forEach(function(field, index){
 
     if(field.name == "gender"){
 
-        console.log("SIM", field)
-    }else{
-        console.log("NAO")
+        if(field.checked) {
+            user[field.name] = field.value;
     }
-//console.log(field)
-
+        
+    }else{
+        user[field.name] = field.value;
+    }
 });
+console.log(user);
