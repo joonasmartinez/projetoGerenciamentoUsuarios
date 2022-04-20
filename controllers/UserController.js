53632ba75e4ec47d6f439c31eca338495351ec19
Jonas <mz-martinez@live.com>
2022-04-20 14:29:23 -0300

    Trocando valor quando True e quando False do Admin

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -112,7 +112,7 @@ class UserController {
         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
+        <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
         <td>${dataUser.birth}</td>
         <td>
         <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
