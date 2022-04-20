a0be6d0a7f17e0a0ed52ea7a6886df530e72cf68
Jonas <mz-martinez@live.com>
2022-04-19 23:04:08 -0300

    Utilizando 'Spread' no elements.ForEach

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,16 +1,31 @@
 class UserController {
 
-    constructor(formid){
+    constructor(formid, tableId){
 
         this.formEl = document.getElementById(formid);
+        this.tableEl = document.getElementById(tableId);
+        
+        this.onSubmit();
+
+    }
+
+    onSubmit(){
+
+        this.formEl.addEventListener("submit", event => {
+
+            event.preventDefault();
+            
+            this.addLine(this.getValues());
+        });
 
     }
 
     getValues(){
 
-        
+        let user = {};
 
-        this.formEl.elements.forEach(function(field, index){
+
+        [...this.formEl.elements].forEach(function(field, index){
 
             if(field.name == "gender"){
         
@@ -23,7 +38,7 @@ class UserController {
             }
         });
     
-        var objectUser = new User(
+        return new User(
             user.name, 
             user.gender, 
             user.birth, 
@@ -33,8 +48,23 @@ class UserController {
             user.photo, 
             user.admin
             );
-
-            return objectUser;
     }
+
+    addLine(dataUser){
+
+    
+       this.tableEl.innerHTML = `<tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+        </tr>`;
+    
+        }
 
 }
