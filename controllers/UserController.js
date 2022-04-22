6382fb3dcc0cad7a51a27277982ea01213987806
Jonas <mz-martinez@live.com>
2022-04-22 16:02:46 -0300

    Marcando checks ao puxar dados

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -164,8 +164,23 @@ class UserController {
             let field = form.querySelector("[name="+name.replace("_","")+"]");
 
             if(field){
-                if(field.type == 'file') continue;
-                field.value = json[name];
+
+                switch(field.type){
+                    case 'file':
+                        continue;
+                    break;
+                    case 'radio':
+                        field = form.querySelector("[name="+name.replace("_","")+"][value="+json[name]+"]");
+                        field.checked = true;
+                    break;
+                    case 'checkbox':
+                        field.checked = json[name]; 
+                    break;
+
+                    default: 
+                        field.value = json[name];
+                }
+                
             }
 
             
