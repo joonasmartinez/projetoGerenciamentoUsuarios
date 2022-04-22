ad8a5db054295cea33bd6f605e741e6078d2b3a7
Jonas <mz-martinez@live.com>
2022-04-21 23:06:00 -0300

    Validando formulario - Campos vazios

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -76,9 +76,17 @@ class UserController {
     getValues(){
 
         let user = {};
-
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function(field, index){
+
+            if(['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+
+
+            }
 
             if(field.name == "gender"){
         
@@ -98,6 +106,10 @@ class UserController {
             }
         });
     
+        if(!isValid){
+            return false;
+        }
+
         return new User(
             user.name, 
             user.gender, 
